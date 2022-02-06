20ed441f394af4ba50d708603b2d14cd60098bd2
Lucas <espinosatrabajo@gmail.com>
2022-02-06 20:16:30 +0100

    changes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
-  return (
-    <>
-        <h3>Agregar TODO</h3>
-        <form>
-            <input type="text"
-            className='form-control mb-2'
-            name="nombre"
-            placeholder='Ingrese ToDo nombre' />
-            <textarea
-            placeholder='form-control mb-2'
-            name="descripcion" 
-            />
-            <select name="estado" className='form-control mb-2'>
-                <option value="pendiente">Pendiente</option>
-                <option value="completado">Completado</option>
-            </select>
-        </form>
-    </>
+
+    const initialState = {
+        nombre: '',
+        descripcion: '',
+        estado: 'pendiente',
+        prioridad: false
+    }
+
+    const [todo, setTodo] = useState(initialState)
+
+    return (
+        <>
+            <h3>Agregar TODO</h3>
+            <form>
+                <input type="text"
+                    className='form-control mb-2'
+                    name="nombre"
+                    placeholder='Ingrese el nombre de su tarea' />
+                <textarea
+                    className="form-control mb-2"
+                    placeholder="Ingrese descripción"
+                    name="descripcion"
+                />
+                <select name="estado" className='form-control mb-2'>
+                    <option value="pendiente">Pendiente</option>
+                    <option value="completado">Completado</option>
+                </select>
+
+                <div className="form-check">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        name="prioridad"
+                        checked=""
+                        id="flexCheckDefault"
+                    />
+                    <label
+                        className="form-check-label"
+                        htmlFor="flexCheckDefault">
+                        Default checkbox
+                    </label>
+                </div>
+                <button
+                    type='submit'
+                    className='btn btn-primary'>Agregar
+                </button>
+            </form>
+        </>
     );
 };
 
